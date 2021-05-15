03ab4cb4514c29db119f6c683278bd6e9f790e53
albarrank <albarrankevin97@gmail.com>
2021-05-14 20:13:18 -0700

    add parseIntoLines function and removeTabSpacing

--- main.js
+++ main.js
@@ -1,1 +1,33 @@
-console.log('Hello world');
+const fs = require('fs');
+
+function parseIntoLines(arr) {
+    return arr.split('\n')
+}
+
+function removeTabSpacing(lines) {       
+    let words = lines.map((line) => line.split('\t'));
+        
+    words = [].concat.apply([], words);
+    words = words.filter(word => word !== '');
+
+    return words;
+}
+
+const readFile = ()=> {
+
+    fs.readFile('./words.txt', 'utf8', (err, data) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+
+        const lines = parseIntoLines(data);
+        const words = removeTabSpacing(lines);
+
+        console.log(words);
+
+
+    })
+}
+
+readFile();
